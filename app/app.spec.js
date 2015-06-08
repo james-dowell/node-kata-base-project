f837b1b4cbf5b20271d62a9f1d19381f2a67d54c
James Dowell <jamesdowell1@gmail.com>
2015-06-08 22:26:09 +0100

    finished the basic node code kata project

--- app/app.spec.js
+++ app/app.spec.js
@@ -1,13 +1,9 @@
-var expect = require('chai').expect;
 var App = require('./app');
-var _ = require('lodash');
-
 
 describe("Something", function(){
 
     it("should be called hello", function(){
         var app = new App();
-        console.log('test');
         expect(app.name).to.equal('hello');
     });
 
--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,19 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
 var files = [
+  './test/helpers.js',
   './app/*.js',
   './app/*.spec.js',
   './app/**/*.js',
 ];
 
 gulp.task('test', function () {
+
     return gulp.src(files, {read: false})
-      .pipe($.mocha({reporter: 'nyan'}));
+        .pipe($.mocha({
+            reporter: 'nyan'
+        }));
+
 });
 
 gulp.task('test:auto', function (done) {

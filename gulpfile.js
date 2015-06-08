ee10432733897e5548edbf58a2f315e8ac80b67a
James Dowell <james@uvd.co.uk>
2015-06-08 14:07:13 +0100

    gulp test now runs the tests :)

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,20 @@ var gulp = require('gulp');
 
 var $ = require('gulp-load-plugins')();
 
+var files = [
+  './app/*.js',
+  './app/*.spec.js',
+  './app/**/*.js',
+];
 
-function runTests (singleRun, done) {
-
-
-  var testFiles = [
-    './app/*.js',
-    './app/*.spec.js',
-    './app/**/*.js',
-  ];
-
-  gulp.src(testFiles)
-    .pipe($.karma({
-      configFile: 'karma.conf.js',
-      action: (singleRun)? 'run': 'watch'
-    }))
-    .on('error', function (err) {
-      // Make sure failed tests cause gulp to exit non-zero
-      throw err;
-    });
-}
-
-gulp.task('default', function () {
-    return gulp.src('test.js', {read: false})
-        // gulp-mocha needs filepaths so you can't have any plugins before it
-        .pipe(mocha({reporter: 'nyan'}));
+gulp.task('test', function () {
+    return gulp.src(files, {read: false})
+      .pipe($.mocha({reporter: 'nyan'}));
 });
 
-gulp.task('test', function (done) { runTests(true /* singleRun */, done) });
-gulp.task('test:auto', function (done) { runTests(false /* singleRun */, done) });
+gulp.task('test:auto', function (done) {
+
+    gulp.task('test:watch', ['test'], reload);
+    gulp.watch(files, ['icons:watch']);
+
+});
